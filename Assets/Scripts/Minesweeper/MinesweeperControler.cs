c3815eb7cd2015b526420e1e9d5a03d0cbc45716
Listek <113714682+XLISTEKX@users.noreply.github.com>
2022-11-06 13:57:31 +0100

    0.0.8c

--- Assets/Scripts/Minesweeper/MinesweeperControler.cs
+++ Assets/Scripts/Minesweeper/MinesweeperControler.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinesweeperControler : MonoBehaviour
 {
-    public byte byt;
+    public int gameAreaSize;
+    public GameObject gameCell;
+    public GameObject cellSpawn;
+    public List<GameObject> allCells;
+
+    private void Start()
+    {
+        float width = cellSpawn.GetComponent<RectTransform>().rect.width / (gameAreaSize +1) ;
+        width -= (gameAreaSize + 1) * 10;
+        float hight = cellSpawn.GetComponent<RectTransform>().rect.height / (gameAreaSize +1);
+        Debug.Log(width + " Width");
+        Debug.Log(cellSpawn.GetComponent<RectTransform>().rect.width + " Gameplay width");
+        //width -= 5 * (gameAreaSize);
+        cellSpawn.GetComponent<GridLayoutGroup>().cellSize = new Vector2(width, hight);
+
+        for(int i = 0; i < gameAreaSize * gameAreaSize; i++)
+        {
+            GameObject temp = Instantiate(gameCell, cellSpawn.transform);
+            allCells.Add(temp);
+        }
+        Debug.Log(width + " Width");
+        Debug.Log(cellSpawn.GetComponent<RectTransform>().rect.width + " Gameplay width");
+    }
+
+
 }
